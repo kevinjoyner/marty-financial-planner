d015fadc117428ed55d3a01bcf90d61f0d550434
Kevin Joyner <joyner.kevin@gmail.com>
2025-12-23 16:47:44 +0000

    startup fixes

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.event import listen
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Use env var for DB location, default to local (Dev-friendly)
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./marty.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data/marty.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
